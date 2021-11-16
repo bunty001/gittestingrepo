a6134dd58224181405c5be3a56d2ea5a72fe1380
shahank <shashankcisen@hotmail.com>
2021-11-16 16:04:45 +0530

    index file commited

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="roundlogo.png">
+<link rel="icon" type="image" href="roundlogo.png">
+<title>Home page- Fitwel Industries LLP</title>
+
+<meta name="description" content="Fitwel Industries LLP is a well-known Manufacturers & Supplier of Titanium Flanges in India & the steel market worldwide. One of our popular products is the Titanium Flanges, more in demand for high-quality production and standard size, specification & dimension."/>
+
+
+
+
+ <link rel="alternate" href="https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html" hreflang="en-in" />
+      <link rel="canonical" href="https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html" />
+      <meta name="author" content="Fitwel Industries LLP" />
+      <meta name="distribution" content="Global"> 
+      <meta name="revisit-after" content="1 days">                 
+      <meta name="copyright" content="Copyright by https://www.fitwelindia.com/">
+      <meta name="robots" content="All,Index,Follow" />                      
+      <meta name="robots" content="noodp,noydir" />
+      <meta name="rating" content="general">
+      <meta name="classification" content="Buttweld Fittings,Forged Fittings & Pipe Flanges">
+      <meta property="og:locale" content="en_in" />
+      <meta property="og:type" content="WebSite" />
+      <meta name="og:email" content="sales@fitwelindia.com"/> 
+      <meta name="og:phone_number" content="+91 9987994248"/>
+      <meta property="og:title" content="Titanium Flanges Manufacturers in India" />
+      <meta property="og:description" content="Fitwel Industries LLP is a well-known Manufacturers & Supplier of Titanium Flanges in India & the steel market worldwide. One of our popular products is the Titanium Flanges, more in demand for high-quality production and standard size, specification & dimension." />
+      <meta property="og:url" content="https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html " />
+      <meta property="og:site_name" content="Fitwel Industries LLP" />
+
+      <script type="application/ld+json">
+         {
+           "@context": "https://schema.org",
+           "@type": "Organization",
+           "url": "https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html ",
+           "logo": "https://www.fitwelindia.com/images/logo-new.png"
+           }
+      </script>
+      <script type="application/ld+json">
+         {
+         "@context": "http://schema.org",
+         "@type": "WebSite",
+         "url": "https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html ",
+         "potentialAction": {
+         "@type": "SearchAction",
+         "target": "https://www.fitwelindia.com/catalogsearch/result/?order=name&dir=desc&q={search_term_string}&x=0&y=0",
+         "query-input": "required name=search_term_string"
+         }
+         }
+      </script>
+      <script type="application/ld+json">
+         { "@context": "http://schema.org/", 
+         "@type": "Organization", 
+         "name": "Fitwel Industries LLP", 
+         "url": "https://www.fitwelindia.com/titanium-gr2-gr5-flanges-manufacturer-exporter.html ", 
+         "image": "https://www.fitwelindia.com/images/tianium-flanges1.jpg", 
+         "description": "Fitwel Industries LLP is a well-known Manufacturers & Supplier of Titanium Flanges in India & the steel market worldwide. One of our popular products is the Titanium Flanges, more in demand for high-quality production and standard size, specification & dimension.", 
+         "aggregateRating": { "@type": "AggregateRating",
+         "ratingValue": "4.3",
+         "reviewCount": "1446", 
+         "bestRating": "5", 
+         "worstRating": "0" } }
+      </script> ab iske age meri file git ka code legi
